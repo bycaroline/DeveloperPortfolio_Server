be108dd9fe6ee8fa11c6fc05850d4f02c1533d1a
bycaroline <eklund.caroline@gmail.com>
2024-05-23 12:19:07 +0200

    application properties removed

--- src/main/java/carolineeklund/portfolio/services/AboutService.java
+++ src/main/java/carolineeklund/portfolio/services/AboutService.java
@@ -1,7 +1,16 @@
 package carolineeklund.portfolio.services;
 
+import carolineeklund.portfolio.repositories.AboutRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AboutService {
+
+    private final AboutRepository aboutRepository;
+
+    @Autowired
+    public AboutService(AboutRepository aboutRepository) {
+        this.aboutRepository = aboutRepository;
+    }
 }
